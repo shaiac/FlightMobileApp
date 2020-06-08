bfa3c5f180b838ae85f6c7f47f7b2efae22e247d
avital levita <avital.levita@live.biu.ac.il>
2020-06-08 21:41:36 +0300

    goes to activity control from connect button

---
+++ app/src/main/java/com/example/flightmobileapp/ControlsActivity.kt
@@ -0,0 +1,26 @@
+package com.example.flightmobileapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.AlarmClock.EXTRA_MESSAGE
+import android.widget.TextView
+import android.widget.Toast
+
+class ControlsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_controls)
+
+        val bundle: Bundle? = intent.extras
+        val id = bundle?.get("id_value")
+        Toast.makeText(applicationContext,id.toString()+" "+id, Toast.LENGTH_LONG).show()
+
+//        // Get the Intent that started this activity and extract the string
+//        val message = intent.getStringExtra(EXTRA_MESSAGE)
+//
+//        // Capture the layout's TextView and set the string as its text
+//        val textView = findViewById<TextView>(R.id.textView).apply {
+//            text = message
+//        }
+    }
+}
--- app/src/main/java/com/example/flightmobileapp/MainActivity.kt
+++ app/src/main/java/com/example/flightmobileapp/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.flightmobileapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    /** Called when the user taps the connect button */
+    fun gotoControl(view: View) {
+        val id:Int = 10 //send "10" to controls
+        val intent = Intent(this, ControlsActivity::class.java)
+        intent.putExtra("id_value", id)
+        startActivity(intent)
     }
 }
