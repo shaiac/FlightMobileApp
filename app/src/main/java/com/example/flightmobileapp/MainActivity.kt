6292f6e2363080fed294bea9d585a235ee074d91
shaiac <shai.acoca@gmail.com>
2020-06-10 17:32:57 +0300

    Click on Lochalhost

--- app/src/main/java/com/example/flightmobileapp/MainActivity.kt
+++ app/src/main/java/com/example/flightmobileapp/MainActivity.kt
@@ -9,10 +9,25 @@ import android.widget.EditText
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var localhost1: TextView
+    private lateinit var localhost2: TextView
+    private lateinit var localhost3: TextView
+    private lateinit var localhost4: TextView
+    private lateinit var localhost5: TextView
+    private lateinit var urlInput: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initViews()
+    }
+
+    fun initViews() {
+        this.urlInput = findViewById(R.id.editTextTextPostalAddress)
+        this.localhost1 = findViewById(R.id.localhost1)
+        this.localhost2 = findViewById(R.id.localhost2)
+        this.localhost3 = findViewById(R.id.localhost3)
+        this.localhost4 = findViewById(R.id.localhost4)
+        this.localhost5 = findViewById(R.id.localhost5)
     }
 
     /** Called when the user taps the connect button */
@@ -24,18 +39,12 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun putUrl(view : View) {
-        var urlInput: EditText = findViewById(R.id.editTextTextPostalAddress)
-        var lochalhost1 : TextView = findViewById(R.id.localhost1)
-        var lochalhost2 : TextView = findViewById(R.id.localhost2)
-        var lochalhost3 : TextView = findViewById(R.id.localhost3)
-        var lochalhost4 : TextView = findViewById(R.id.localhost4)
-        var lochalhost5 : TextView = findViewById(R.id.localhost5)
         when (view.id) {
-            R.id.localhost1 -> urlInput.setText(lochalhost1.text)
-            R.id.localhost2 -> urlInput.setText(lochalhost2.text)
-            R.id.localhost3 -> urlInput.setText(lochalhost3.text)
-            R.id.localhost4 -> urlInput.setText(lochalhost4.text)
-            else -> urlInput.setText(lochalhost5.text)
+            R.id.localhost1 -> urlInput.setText(localhost1.text)
+            R.id.localhost2 -> urlInput.setText(localhost2.text)
+            R.id.localhost3 -> urlInput.setText(localhost3.text)
+            R.id.localhost4 -> urlInput.setText(localhost4.text)
+            else -> urlInput.setText(localhost5.text)
         }
 
 
