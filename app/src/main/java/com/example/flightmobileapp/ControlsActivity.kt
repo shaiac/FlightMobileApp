545af088478c6221c58ad86d517f5bee241886be
green <green@LAPTOP-SR93CP99>
2020-06-11 14:53:55 +0300

    value of sliders

--- app/src/main/java/com/example/flightmobileapp/ControlsActivity.kt
+++ app/src/main/java/com/example/flightmobileapp/ControlsActivity.kt
@@ -4,9 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SeekBar
+import android.widget.Toast
 import io.github.controlwear.virtual.joystick.android.JoystickView
 import kotlinx.android.synthetic.main.activity_controls.*
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_main.view.*
+import kotlinx.android.synthetic.main.activity_main.view.textView
 import kotlinx.android.synthetic.main.activity_second.*
 import kotlinx.android.synthetic.main.activity_second.joystickView
 
@@ -15,7 +19,53 @@ class ControlsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_controls)
         setJoystick()
-        }
+
+        val seek = findViewById<SeekBar>(R.id.seekBar3)
+        seek?.setOnSeekBarChangeListener(object :
+            SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(
+                seek: SeekBar,
+                progress: Int, fromUser: Boolean
+            ) {
+
+                // write custom code for progress is changed
+            }
+
+            override fun onStartTrackingTouch(seek: SeekBar) {
+                val a = seek.progress
+                seekBarText3.setText(seek.progress.toString())
+                // write custom code for progress is started
+            }
+
+            override fun onStopTrackingTouch(seek: SeekBar) {
+                // write custom code for progress is stopped
+            }
+        })
+
+        val seek2 = findViewById<SeekBar>(R.id.seekBar)
+        seek2?.setOnSeekBarChangeListener(object :
+            SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(
+                seek2: SeekBar,
+                progress: Int, fromUser: Boolean
+            ) {
+
+                // write custom code for progress is changed
+            }
+
+            override fun onStartTrackingTouch(seek2: SeekBar) {
+                val a = seek2.progress
+                seekBarText.setText(seek2.progress.toString())
+                // write custom code for progress is started
+            }
+
+            override fun onStopTrackingTouch(seek: SeekBar) {
+                // write custom code for progress is stopped
+            }
+        })
+
+
+    }
 
     private fun setJoystick() {
         val joystick = joystickView.right
