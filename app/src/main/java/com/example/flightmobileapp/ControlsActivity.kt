f2e4891f619aa321dc5443b3ec2117b4dcb6130c
avital levita <avital.levita@live.biu.ac.il>
2020-06-11 17:35:03 +0300

    sliders

--- app/src/main/java/com/example/flightmobileapp/ControlsActivity.kt
+++ app/src/main/java/com/example/flightmobileapp/ControlsActivity.kt
@@ -1,68 +1,65 @@
 package com.example.flightmobileapp
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.view.ViewGroup
 import android.widget.SeekBar
-import android.widget.Toast
-import io.github.controlwear.virtual.joystick.android.JoystickView
+import com.ramotion.fluidslider.FluidSlider
 import kotlinx.android.synthetic.main.activity_controls.*
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.activity_main.view.*
-import kotlinx.android.synthetic.main.activity_main.view.textView
-import kotlinx.android.synthetic.main.activity_second.*
 import kotlinx.android.synthetic.main.activity_second.joystickView
+import java.math.RoundingMode
 
 class ControlsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_controls)
         setJoystick()
+        setSliders()
 
-        val seek = findViewById<SeekBar>(R.id.seekBar3)
-        seek?.setOnSeekBarChangeListener(object :
-            SeekBar.OnSeekBarChangeListener {
-            override fun onProgressChanged(
-                seek: SeekBar,
-                progress: Int, fromUser: Boolean
-            ) {
 
-                // write custom code for progress is changed
-            }
-
-            override fun onStartTrackingTouch(seek: SeekBar) {
-                val a = seek.progress
-                seekBarText3.setText(seek.progress.toString())
-                // write custom code for progress is started
-            }
-
-            override fun onStopTrackingTouch(seek: SeekBar) {
-                // write custom code for progress is stopped
-            }
-        })
-
-        val seek2 = findViewById<SeekBar>(R.id.seekBar)
-        seek2?.setOnSeekBarChangeListener(object :
-            SeekBar.OnSeekBarChangeListener {
-            override fun onProgressChanged(
-                seek2: SeekBar,
-                progress: Int, fromUser: Boolean
-            ) {
-
-                // write custom code for progress is changed
-            }
-
-            override fun onStartTrackingTouch(seek2: SeekBar) {
-                val a = seek2.progress
-                seekBarText.setText(seek2.progress.toString())
-                // write custom code for progress is started
-            }
-
-            override fun onStopTrackingTouch(seek: SeekBar) {
-                // write custom code for progress is stopped
-            }
-        })
+//        val seek = findViewById<SeekBar>(R.id.throttleSlider)
+//        seek?.setOnSeekBarChangeListener(object :
+//            SeekBar.OnSeekBarChangeListener {
+//            override fun onProgressChanged(
+//                seek: SeekBar,
+//                progress: Int, fromUser: Boolean
+//            ) {
+//
+//                // write custom code for progress is changed
+//            }
+//
+//            override fun onStartTrackingTouch(seek: SeekBar) {
+//                val a = seek.progress
+//                seekBarText3.setText(seek.progress.toString())
+//                // write custom code for progress is started
+//            }
+//
+//            override fun onStopTrackingTouch(seek: SeekBar) {
+//                // write custom code for progress is stopped
+//            }
+//        })
+//
+//        val seek2 = findViewById<SeekBar>(R.id.rudderSlider)
+//        seek2?.setOnSeekBarChangeListener(object :
+//            SeekBar.OnSeekBarChangeListener {
+//            override fun onProgressChanged(
+//                seek2: SeekBar,
+//                progress: Int, fromUser: Boolean
+//            ) {
+//
+//                // write custom code for progress is changed
+//            }
+//
+//            override fun onStartTrackingTouch(seek2: SeekBar) {
+//                val a = seek2.progress
+//                seekBarText.setText(seek2.progress.toString())
+//                // write custom code for progress is started
+//            }
+//
+//            override fun onStopTrackingTouch(seek: SeekBar) {
+//                // write custom code for progress is stopped
+//            }
+//        })
 
 
     }
@@ -74,23 +71,39 @@ class ControlsActivity : AppCompatActivity() {
             val aileron = kotlin.math.cos(Math.toRadians(angle.toDouble())) * strength / 100
             val elevator = kotlin.math.sin(Math.toRadians(angle.toDouble())) * strength / 100
 
-            aileronText.setText((aileron.toDouble()).toString())
+            aileronText.setText(aileron.toDouble().toString())
             elevatorText.setText(elevator.toDouble().toString())
 
-
             // send aileron and elevator to command server
-
     }
 }
 
+    private fun setSliders() {
+        // set rudder slider (seek bar)
+        val rudderSeek = findViewById<FluidSlider>(R.id.fluidSliderRudder)
+        rudderSeek.bubbleText = "0"
+        val maxR = 1
+        val minR = -1
+        val totalR = maxR - minR
+        rudderSeek.positionListener = { p ->
+            val rudder = minR + (totalR * p)
+            // send rudder to server
+            val rudderDisplay = rudder.toBigDecimal().setScale(2, RoundingMode.UP).toDouble()
+            rudderSeek.bubbleText = rudderDisplay.toString()
+        }
+        // set throttle slider (seek bar)
+        val throttleSeek = findViewById<FluidSlider>(R.id.fluidSliderThrottle)
+        val maxT = 1
+        val minT = 0
+        val totalT = maxT - minT
+        throttleSeek.positionListener = { p ->
+            val throttle = minT + (totalT * p)
+            // send throttle to server
+            val throttleDisplay = throttle.toBigDecimal().setScale(2, RoundingMode.UP).toDouble()
+            throttleSeek.bubbleText = throttleDisplay.toString()
+        }
+    }
 
 
-//        // Get the Intent that started this activity and extract the string
-//        val message = intent.getStringExtra(EXTRA_MESSAGE)
-//
-//        // Capture the layout's TextView and set the string as its text
-//        val textView = findViewById<TextView>(R.id.textView).apply {
-//            text = message
-//        }
 }
 
